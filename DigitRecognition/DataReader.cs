943af8feea2fffc126977cb2c4b34b77b2dff738
Konstantin Finagin <konstantin.finagin@dataart.com>
2016-11-04 21:09:15 +0300

    initial commit

---
+++ DigitRecognition/DataReader.cs
@@ -0,0 +1,27 @@
+﻿namespace DigitRecognition
+{
+    using System;
+    using System.Linq;
+
+    public class DataReader
+    {
+        private static Observation ObservationFactory(string data)
+        {
+            var commaSeparated = data.Split(',');
+
+            var label = commaSeparated[0];
+
+            var pixels = commaSeparated
+                .Skip(1)
+                .Select(x => Convert.ToInt32(x))
+                .ToArray();
+
+            return new Observation(label, pixels);
+        }
+
+        public static Observation[] ReadObservations(string dataPath)
+        {
+            
+        }
+    }
+}
---
+++ DigitRecognition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
