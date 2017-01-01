ff0d8b1af4d2c43b3e81c111149999b30422a8c5
Konstantin Finagin <konstantin.finagin@dataart.com>
2017-01-01 12:37:11 +0300

    -

--- DigitRecognitionFSharp/Script.fsx
+++ DigitRecognitionFSharp/Script.fsx
@@ -1,8 +1,17 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿open System.IO
 
-#load "Library1.fs"
-open DigitRecognitionFSharp
+type Observation = { Label:string; Pixels:int[] }
 
-// Define your library scripting code here
+let toObservation (csvData:string) = 
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
 
+let reader path = 
+    let data = File.ReadAllLines path
+    data.[1..] 
+    |> Array.map toObservation
+
+let trainingPath = @"D:\Learning\MachineLearningNet\DigitRecognition\Data\trainingSample.csv"
+let trainingData = reader trainingPath
