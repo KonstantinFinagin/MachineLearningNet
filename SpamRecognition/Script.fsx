346fa6ba57cd19e5c2d26c8093207ae4c3a20b60
Konstantin Finagin <konstantin.finagin@dataart.com>
2017-01-07 17:10:54 +0300

    simplest classification done

--- SpamRecognition/Script.fsx
+++ SpamRecognition/Script.fsx
@@ -1,15 +1,52 @@
 ﻿#load "NaiveBayes.fs"
 
+open System.IO
 open NaiveBayes.Classifier
 open System.Text.RegularExpressions
+
+type DocType = 
+    | Ham
+    | Spam 
+
+let parseDocType (label:string) = 
+    match label with
+    | "ham" -> Ham
+    | "spam" -> Spam
+    | _ -> failwith "Unknown label"
+   
+let parseLine (line:string) = 
+    let split = line.Split('\t')
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
+
+let fileName = "SMSSpamCollection"
+let path = __SOURCE_DIRECTORY__ + @"..\..\Data\" + fileName
+
+let dataSet = 
+    File.ReadAllLines path
+    |> Array.map parseLine
+
+// use first 1000 for training and the rest for classification
+let training = 
+    dataSet |> Array.take 1000
+
+let validation = 
+    dataSet |> Array.skip 1000
 
 let matchWords = Regex(@"\w+")
 
 // tokenizing a line of text with regular expressions
-let tokens (text : string) = 
+let wordTokenizer (text : string) = 
     text.ToLowerInvariant()
     |> matchWords.Matches
     |> Seq.cast<Match>
     |> Seq.map (fun m -> m.Value)
     |> Set.ofSeq
 
+let txtClassifier = train training wordTokenizer (["txt"] |> set)
+
+validation 
+|> Seq.averageBy (fun (docType, sms) -> 
+    if docType = txtClassifier sms then 1.0 else 0.0)
+|> printfn "Based on 'txt', correcly classified: %.3f"
