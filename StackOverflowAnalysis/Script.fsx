4d3c811f030ad3464a76a964c07ab73c7e080d8a
Konstantin Finagin <konstantin.finagin@dataart.com>
2017-01-11 19:07:12 +0300

    using simple DSL to analyze tags from SO

--- StackOverflowAnalysis/Script.fsx
+++ StackOverflowAnalysis/Script.fsx
@@ -29,3 +29,44 @@ type HardCodedQuestions = JsonProvider<sample>
 
 let javaQuery = "https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=java"
 let javaQuestions = HardCodedQuestions.Load(javaQuery)
+
+let questionQuery = """https://api.stackexchange.com/2.2/questions?site=stackoverflow"""
+
+// Implementing simple DSL for queries
+
+let tagged tags query =
+    // join the tags in a ; separated string
+    let joinedTags = tags |> String.concat ";"
+    sprintf "%s&tagged=%s" query joinedTags
+
+let page p query = sprintf "%s&page=%i" query p
+
+let pageSize s query = sprintf "%s&pagesize=%i" query s
+
+let extractQuestions (query : string) = Questions.Load(query).Items
+
+let ``C#`` = "C%23"
+let ``F#`` = "F%23"
+
+let fsSample = 
+    questionQuery
+    |> tagged [``F#``]
+    |> pageSize 100
+    |> extractQuestions
+
+let csSample =
+    questionQuery
+    |> tagged [``C#``]
+    |> pageSize 100
+    |> extractQuestions
+
+let analyzeTags (qs:Questions.Item seq) =
+    qs
+    |> Seq.collect (fun question -> question.Tags)
+    |> Seq.countBy id
+    |> Seq.filter (fun (_, count) -> count > 2)
+    |> Seq.sortBy (fun (_, count) -> -count)
+    |> Seq.iter (fun (tag, count) -> printfn "%s, %i" tag count)
+
+analyzeTags fsSample
+analyzeTags csSample
