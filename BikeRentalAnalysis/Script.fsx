9105a4a00bec815855454dda5a09ea582ae80325
Konstantin Finagin <konstantin.finagin@dataart.com>
2017-01-19 23:09:09 +0300

    -

---
+++ BikeRentalAnalysis/Script.fsx
@@ -0,0 +1,16 @@
+﻿#I @"..\packages\"
+#r @"FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+#r @"FSharp.Charting.0.90.14\lib\net40\FSharp.Charting.dll"
+
+open FSharp.Data
+open FSharp.Charting
+
+[<Literal>]
+let path = __SOURCE_DIRECTORY__ + @"\..\Data\" + "day.csv"
+
+type Data = CsvProvider<path>
+let dataSet = Data.Load(path)
+let data = dataSet.Rows
+
+let all = Chart.Line [ for obs in data -> obs.Cnt ]
+
