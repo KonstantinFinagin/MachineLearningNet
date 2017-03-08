0735e494f479e58022916e73cca94044df36d415
Konstantin Finagin <konstantin.finagin@dataart.com>
2017-03-08 15:53:53 +0300

    dataset

---
+++ StackoverflowPrincipalComponents/Script.fsx
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+
+let folder = __SOURCE_DIRECTORY__
+let file = "userprofiles-toptags.txt"
+
+let headers, observations = 
+
+    let raw = 
+        folder + "/" + file
+        |> File.ReadAllLines
+
+    let headers = (raw.[0].Split ',').[1..]
+
+    let observations = 
+        raw.[1..]
+        |> Array.map (fun line -> (line.Split ',').[1..])
+        |> Array.map (Array.map float)
+
+    headers, observations
+
